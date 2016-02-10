f887a6f390330412faf868a8aaf59879d82b9470
Jesse Tipton <me@jessetipton.com>
2016-02-09 20:20:17 -0600

    added workshop 2 end state

---
+++ 2 - UITableView Is in the Building/Buildings (End)/Buildings/BuildingDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BuildingDetailViewController.swift
+//  Buildings
+//
+//  Created by Jesse Tipton on 2/9/16.
+//  Copyright © 2016 MAD. All rights reserved.
+//
+
+import UIKit
+
+class BuildingDetailViewController: UIViewController {
+
+}
