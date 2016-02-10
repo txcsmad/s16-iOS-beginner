1cf12c9a30237e005a41f5402d74ff0e64fef1cc
Jesse Tipton <me@jessetipton.com>
2016-02-09 18:40:28 -0600

    workshop 2

---
+++ 2 - UITableView Is in the Building/Buildings (Start)/Buildings/BuildingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BuildingsViewController.swift
+//  Buildings
+//
+//  Created by Jesse Tipton on 2/9/16.
+//  Copyright © 2016 MAD. All rights reserved.
+//
+
+import UIKit
+
+class BuildingsViewController: UITableViewController {
+    
+}
