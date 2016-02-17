13d69e67783a1cb998bd94fc5e47a9668f3d61b1
Jesse Tipton <me@jessetipton.com>
2016-02-16 20:14:46 -0600

    added end state

---
+++ 3 - The Devil is in the Details/Details (End)/Buildings/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Buildings
+//
+//  Created by Jesse Tipton on 2/9/16.
+//  Copyright © 2016 MAD. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
